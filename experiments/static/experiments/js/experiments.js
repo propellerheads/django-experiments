c8da7991f8b7e3df2601a46f26070843c721947a
Jamie Robertson <jamie@jamierobertson.io>
2017-06-29 15:15:29 +0200

    Check if using $.cookie or newer js-cookies before setting

--- experiments/static/experiments/js/experiments.js
+++ experiments/static/experiments/js/experiments.js
@@ -10,17 +10,21 @@ experiments = function() {
         }
     };
 }();
+!(function() {
+    // using jQuery cookie or js-cookie ?
+    var setCookie = $.cookie || Cookies.set;
 
-if (document.addEventListener) {
-    // sets the cookie in the capturing phase so that in the bubbling phase we guarantee that if a request is being issued it will contain the new cookie as well
-    document.addEventListener("click", function(event) {
-        if ((event.target).hasAttribute('data-experiments-goal')) {
-            $.cookie("experiments_goal", $(event.target).data('experiments-goal'), { path: '/' });
-        }
-    }, true);
-} else { // IE 8
-    $(document).delegate('[data-experiments-goal]', 'click', function(e) {
-        // if a request is fired by the click event, the cookie might get set after it, thus the goal will be recorded with the next request (if there will be one)
-        $.cookie("experiments_goal", $(this).data('experiments-goal'), { path: '/' });
-    });
-}
+    if (document.addEventListener) {
+        // sets the cookie in the capturing phase so that in the bubbling phase we guarantee that if a request is being issued it will contain the new cookie as well
+        document.addEventListener("click", function(event) {
+            if ((event.target).hasAttribute('data-experiments-goal')) {
+                setCookie("experiments_goal", $(event.target).data('experiments-goal'), { path: '/' });
+            }
+        }, true);
+    } else { // IE 8
+        $(document).delegate('[data-experiments-goal]', 'click', function(e) {
+            // if a request is fired by the click event, the cookie might get set after it, thus the goal will be recorded with the next request (if there will be one)
+            setCookie("experiments_goal", $(this).data('experiments-goal'), { path: '/' });
+        });
+    }
+})();
